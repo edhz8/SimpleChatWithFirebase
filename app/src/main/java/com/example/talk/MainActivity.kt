39f19832de33250a44d374dd1b77177bbae74afb
edhz8888 <edhz8888@gmail.com>
2020-07-31 17:50:51 +0900

    Initial commit

---
+++ app/src/main/java/com/example/talk/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.example.talk
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    var googleSignInClient : GoogleSignInClient? = null
+    val RC_SIGN_IN = 1000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        signup_button.setOnClickListener {
+            createEmailId()
+        }
+
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+        googleSignInClient = GoogleSignIn.getClient(this,gso)
+
+        google_login_button.setOnClickListener {
+            var signInIntent = googleSignInClient?.signInIntent
+            startActivityForResult(signInIntent,RC_SIGN_IN)
+        }
+
+
+    }
+
+
+
+    fun createEmailId(){
+        var email = email_edittext.text.toString()
+        var password = password_edittext.text.toString()
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,password).addOnCompleteListener { task ->
+            if(task.isSuccessful){
+                println("signup success")
+            }
+        }
+    }
+
+    fun firebaseAuthWithGoogle(acct : GoogleSignInAccount?){
+        var credential = GoogleAuthProvider.getCredential(acct?.idToken,null)
+        FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener {
+            task ->
+            if(task.isSuccessful){
+                println("Google Login Success")
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == RC_SIGN_IN){
+            var task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            var account = task.getResult(ApiException::class.java)
+            firebaseAuthWithGoogle(account)
+        }
+    }
+
+}
