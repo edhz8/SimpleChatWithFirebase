9a200295878c8a3e1128c2a8b72779e5d1cc3880
hj56775 <68840193+hj56775@users.noreply.github.com>
2020-08-06 16:26:40 +0900

    fragment전환

--- app/src/main/java/com/example/talk/MainActivity.kt
+++ app/src/main/java/com/example/talk/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.talk
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.auth.api.Auth
 import com.google.android.gms.auth.api.signin.GoogleSignIn
@@ -10,12 +11,31 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),BottomNavigationView.OnNavigationItemSelectedListener {
     var googleSignInClient : GoogleSignInClient? = null
+
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        when(p0.itemId) {
+            R.id.home->{
+                val transaction =supportFragmentManager.beginTransaction()
+                transaction.replace(R.id.frame_Layout,HomeFragment())
+                transaction.commit()
+                return true
+        }
+          R.id.chat-> {
+              val transaction =supportFragmentManager.beginTransaction()
+              transaction.replace(R.id.frame_Layout,ChatFragment())
+              transaction.commit()
+              return true
+          }
+    }
+        return false
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -25,14 +45,16 @@ class MainActivity : AppCompatActivity() {
             .requestEmail()
             .build()
         googleSignInClient = GoogleSignIn.getClient(this,gso)
-        logout_button.setOnClickListener {
+        bottomNavigationView.setOnNavigationItemSelectedListener(this)
+        /*logout_button.setOnClickListener {
             logout()
             startActivity(Intent(this, LoginActivity::class.java))
         }
         profile_edit_button.setOnClickListener {
             startActivity(Intent(this, profileEditActivity::class.java))
             this.finish()
-        }
+        }*/
+
     }
 
     fun logout(){
